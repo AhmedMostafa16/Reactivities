eff28511425c315684dba90f5c23610608e36290
Ahmed Mostafa <amk67482@gmail.com>
2022-02-05 02:27:42 +0200

    Added basic CRUD functionalities + prepared UI

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection service, IConfiguration config)
+    {
+        service.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPIv5", Version = "v1"}); });
+        service.AddDbContext<DataContext>(opt => { opt.UseSqlite(config.GetConnectionString("DefaultConnection")); });
+        service.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy",
+                policy => { policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); });
+        });
+        service.AddMediatR(typeof(List.Handler).Assembly);
+        service.AddAutoMapper(typeof(MappingProfiles).Assembly);
+        return service;
+    }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +6,9 @@ namespace Persistence;
 // Represent a session with the underlying database using which you can perform CRUD operations. 
 public class DataContext : DbContext
 {
-    public DataContext(DbContextOptions options) : base(options) { }
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
 
     public DbSet<Activity> Activities { get; set; }
 }
